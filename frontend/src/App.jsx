bd2af3efcd6bdca0537335542c1bf3f6225be889
dburnham1212 <dburnham1212@gmail.com>
2023-07-13 14:05:44 -0400

    Creating home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import PhotoList from './components/PhotoList';
-import TopNavigation from './components/TopNavigationBar';
+import HomeRoute from './routes/HomeRoute';
 
 import './App.scss';
 
@@ -9,8 +8,7 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <TopNavigation />
-    <PhotoList /> 
+    <HomeRoute />
   </div>
 )
 
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import TopNavigation from '../components/TopNavigationBar';
+import PhotoList
+ from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => 
-  <div className="home-route">
-    {/* Insert React */}
-  </div>
+const HomeRoute = () => {
+  return(
+    <div className="home-route">
+      <TopNavigation />
+      <PhotoList /> 
+    </div>
+  )
+  
+}
 
 export default HomeRoute;
