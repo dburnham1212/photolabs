51507c91c6f98bec8f24c2660b2ae1ca924d28a1
dburnham1212 <dburnham1212@gmail.com>
2023-07-13 13:57:07 -0400

    Adding navbar to page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import TopNavigation from './components/TopNavigationBar';
 
 import './App.scss';
 
@@ -9,7 +9,7 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <TopicList />
+    <TopNavigation />
     <PhotoList /> 
   </div>
 )
