0fe03a296f7e75548f1850148b7ba675e470454a
dburnham1212 <dburnham1212@gmail.com>
2023-07-15 17:35:26 -0400

    Linking photos and topics with database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import useApplicationData from './hooks/useApplicationData';
 
 import HomeRoute from './routes/HomeRoute';
 import PhotoDetailsModal from './routes/PhotoDetailsModal'
-import topics from './mocks/topics';
-import photos from './mocks/photos';
 
 import './App.scss';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/photos')
+      .then(res => res.json())
+      .then(data => { setPhotos([...data])
+   })
+   }, [photos])
+
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/topics')
+      .then(res => res.json())
+      .then(data => { setTopics([...data])
+   })
+   }, [topics])
+
   const {
     state,
     clickPhoto,
